cd0089ebbd82daba27bd3c83a364ea8a285960c8
gil <gsqwow@gmail.com>
2023-09-09 15:21:38 -0700

    Add options to navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,15 @@
 const Nav = () => {
     return (
         <div className='nav-main'>
-            <ul>
-                <li>Home</li>
-            </ul>
+            <div>
+                <div className='logo'></div>
+                <p>Peripherals</p>
+            </div>
+            <div><p>Search</p></div>
+            <div className='nav-right'>
+                <button>Subscribe</button>
+                <button>Join</button>
+            </div>
         </div>
     )
 }
