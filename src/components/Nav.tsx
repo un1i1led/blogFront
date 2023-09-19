cf65a7ce06a98bba6267136a75da1657060c4aad
gil <gsqwow@gmail.com>
2023-09-18 20:43:52 -0700

    Add nav bar structure

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,23 @@
+import notifImg from '../assets/notification-14-svgrepo-com.svg';
+import hamburger from '../assets/hamburger-svgrepo-com.svg';
+
 const Nav = () => {
     return (
         <div className='nav-main'>
-            <div>
-                <div className='logo'></div>
-                <p>Peripherals</p>
+            <div className='nav nav-left'>
+                <div>
+                    <img className='small-img' src={hamburger} alt=''/>
+                </div>
             </div>
-            <div><p>Search</p></div>
-            <div className='nav-right'>
-                <button>Subscribe</button>
-                <button>Join</button>
+            <div className='nav nav-mid'>
+                <div className='mid-block'>
+                    <p>Blog</p>
+                </div>
+            </div>
+            <div className='nav nav-right'>
+                <div>
+                    <img className='mid-img' src={notifImg} alt='notifications'/>
+                </div>
             </div>
         </div>
     )
