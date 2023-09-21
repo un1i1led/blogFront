f2be60bc1b008d9daceedb4a0515b29d02de9d08
gil <gsqwow@gmail.com>
2023-09-21 13:47:44 -0700

    Add article feed component

---
+++ src/components/ArticleFeed.tsx
@@ -0,0 +1,11 @@
+import CategoriesSlider from "./CategoriesSlider";
+
+const ArticleFeed = () => {
+    return (
+        <div className='feed'>
+            <CategoriesSlider/>
+        </div>
+    )
+}
+
+export default ArticleFeed;
