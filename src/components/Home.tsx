f55d1a362617a973c80eb01907523fc42c8e19df
gil <gsqwow@gmail.com>
2023-09-21 13:50:31 -0700

    Add article feed to homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,11 @@
+import ArticleFeed from "./ArticleFeed";
 import Spotlight from "./Spotlight";
 
 const Home = () => {
     return (
         <div className='home-main'>
             <Spotlight/>
+            <ArticleFeed/>
         </div>
     )
 }
