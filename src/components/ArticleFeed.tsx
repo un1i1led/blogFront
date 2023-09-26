d73ada2d04b74e6fe1a4d970c3b09eef36b05fa9
gil <gsqwow@gmail.com>
2023-09-26 09:22:19 -0700

    Add category fetch func

--- src/components/ArticleFeed.tsx
+++ src/components/ArticleFeed.tsx
@@ -1,9 +1,40 @@
 import CategoriesSlider from "./CategoriesSlider";
+import { useState, useEffect } from 'react';
+
+interface category {
+    id: string;
+    name: string;
+    name_lowered: string;
+}
 
 const ArticleFeed = () => {
+    const [categories, setCategories] = useState<category[]>([{id: '0', name: 'All', name_lowered: 'all'}]);
+    const [category, setCategory] = useState('All');
+
+    const changeCategory = (name:string) => {
+        setCategory(name);
+    }
+
+    useEffect(() => {
+        async function fetchCategories() {
+            const newCategories: category[] = []
+
+            await fetch('http://localhost:3000/tagslider')
+                .then(res => res.json())
+                .then(res => {
+                    res.tags.map((data) => newCategories.push(data))
+                })
+
+            setCategories([...categories, ...newCategories])
+        }
+        
+        fetchCategories();
+
+    }, []);
+
     return (
         <div className='feed'>
-            <CategoriesSlider/>
+            <CategoriesSlider categories={categories} category={category} changeCategory={changeCategory}/>
         </div>
     )
 }
