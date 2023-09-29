4f89960b153f708a33302428ad7f4645dfc13e8a
gil <gsqwow@gmail.com>
2023-09-28 21:51:54 -0700

    Modify prop and category types

--- src/components/CategoriesSlider.tsx
+++ src/components/CategoriesSlider.tsx
@@ -1,13 +1,13 @@
 import { v4 as uuidv4 } from 'uuid';
 
 interface sliderDetails {
-    categories: Array<category>;
-    category: string;
-    changeCategory(name:string): void;
+    categories: Array<Category>;
+    category: Category;
+    changeCategory(newCategory: Category): void;
 }
 
-interface category {
-    id: string;
+interface Category {
+    _id: string;
     name: string;
     name_lowered: string;
 }
@@ -16,7 +16,7 @@ const CategoriesSlider = (props: sliderDetails) => {
 
     const popCats = () => {
        const items = props.categories.map(data => 
-            <li key={uuidv4()} className={props.category == data.name ? 'cat-selected' : 'cat-unselected'} onClick={() => props.changeCategory(data.name_lowered)}>{data.name}</li>
+            <li key={uuidv4()} className={props.category.name_lowered == data.name_lowered ? 'cat-selected' : 'cat-unselected'} onClick={() => props.changeCategory(data)}>{data.name}</li>
         );
 
         return <ul className='cat-list'>{items}</ul>
