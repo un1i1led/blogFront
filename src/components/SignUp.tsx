e2099f8606e6d992c5371435b335d0ea9c2af32b
gil <gsqwow@gmail.com>
2023-10-07 10:53:08 -0700

    Add notif when signup succesful

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { v4 as uuid } from 'uuid';
+import { useNavigate } from 'react-router-dom';
 
 interface Error {
     location: string;
@@ -16,6 +17,9 @@ const SignUp = () => {
     const [username, setUsername] = useState<string>('');
     const [name, setName] = useState<string>('');
     const [errors, setErrors] = useState<Error[]>([]);
+    const [activeUser, setActiveUser] = useState<boolean>(false);
+
+    const navigate = useNavigate();
 
     const postUser = () => {
         const formData = { name, username, email, password, confirm };
@@ -32,29 +36,38 @@ const SignUp = () => {
                     res.errors.map((data:Error) => arr.push(data))
                     setErrors([...arr]);
                 } else {
-                    console.log(res.data);
+                    setActiveUser(true);
+                    setTimeout(() => {
+                        navigate('/');
+                    }, 3000);
                 }
             })
+    }
+
+    const renderForm = () => {
+        return (
+            <form action='' className='signup-form'>
+                <label htmlFor='name-signup'>Name <span className='red'>*</span></label>
+                <input type='text' name='name' id='name-signup' required onChange={(e) => setName(e.target.value)}/>
+                <label htmlFor='username-signup'>Username <span className='red'>*</span></label>
+                <input type='text' name='username' id='username-signup' required onChange={(e) => setUsername(e.target.value)}/>
+                <label htmlFor='email-signup'>Email <span className='red'>*</span></label>
+                <input type='email' name='email' id='email-signup' required onChange={(e) => setEmail(e.target.value)}/>
+                <label htmlFor='password-signup'>Password <span className='red'>*</span></label>
+                <input type='password' name='password' id='password-signup' required onChange={(e) => setPassword(e.target.value)}/>
+                <label htmlFor='confirm-signup'>Password Confirmation <span className='red'>*</span></label>
+                <input type='password' name='confirm' id='confirm-signup' required onChange={(e) => setConfirm(e.target.value)}/>
+                <button type='button' onClick={postUser}>Sign up</button>
+                {errors.length > 0 ? errors.map((data) => <p className='red' key={uuid()}>{data.msg}</p>) : ''}
+            </form>
+        )
     }
 
     return (
         <div className='signup-main'>
             <h1>Create your account</h1>
-            <form action='' className='signup-form'>
-                <label htmlFor='name'>Name <span className='red'>*</span></label>
-                <input type='text' name='name' id='name-signup' required onChange={(e) => setName(e.target.value)}/>
-                <label htmlFor='username'>Username <span className='red'>*</span></label>
-                <input type='text' name='username' id='username-signup' required onChange={(e) => setUsername(e.target.value)}/>
-                <label htmlFor='email'>Email <span className='red'>*</span></label>
-                <input type='email' name='email' id='email-signup' required onChange={(e) => setEmail(e.target.value)}/>
-                <label htmlFor='password'>Password <span className='red'>*</span></label>
-                <input type='password' name='password' id='password-signup' required onChange={(e) => setPassword(e.target.value)}/>
-                <label htmlFor='confirm'>Password Confirmation <span className='red'>*</span></label>
-                <input type='password' name='confirm' id='confirm-signup' required onChange={(e) => setConfirm(e.target.value)}/>
-                <button type='button' onClick={postUser}>Sign up</button>
-                {errors.length > 0 ? errors.map((data) => <p className='red' key={uuid()}>{data.msg}</p>) : ''}
-            </form>
-            <a href='/login'>Already have an account? <span className='third'>Log in</span></a>
+            {activeUser ? <div className='centered'><h2>Account created! Redirecting now...</h2></div> : renderForm()}
+            {activeUser ? '' : <a href='/login'>Already have an account? <span className='third'>Log in</span></a>}
         </div>
     )
 }
