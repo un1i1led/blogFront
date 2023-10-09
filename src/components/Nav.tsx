e5142b4c8257465609d96b328f35468213ea23ae
gil <gsqwow@gmail.com>
2023-10-08 22:56:24 -0700

    Add different navs depending if logged in

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,26 +2,65 @@ import notifImg from '../assets/notification-14-svgrepo-com.svg';
 import hamburger from '../assets/hamburger-svgrepo-com.svg';
 import { Link } from 'react-router-dom';
 
-const Nav = () => {
-    return (
-        <div className='nav-main'>
-            <div className='nav nav-left'>
-                <div>
-                    <img className='small-img' src={hamburger} alt=''/>
-                </div>
-            </div>
-            <div className='nav nav-mid'>
-                <Link to={'/'}>
-                    <div className='mid-block'>
-                        <p>Blog</p>
+interface NavDetails {
+    hasToken: boolean;
+}
+
+const Nav = (props: NavDetails) => {
+    const popWhenToken = () => {
+        return (
+            <div className='nav-main'>
+                <div className='nav nav-left'>
+                    <div>
+                        <img className='small-img' src={hamburger} alt=''/>
                     </div>
-                </Link>
-            </div>
-            <div className='nav nav-right'>
-                <div>
-                    <img className='mid-img' src={notifImg} alt='notifications'/>
+                </div>
+                <div className='nav nav-mid'>
+                    <Link to={'/'}>
+                        <div className='mid-block'>
+                            <p>Blog</p>
+                        </div>
+                    </Link>
+                </div>
+                <div className='nav nav-right'>
+                    <div>
+                        <img className='mid-img' src={notifImg} alt='notifications'/>
+                        <div className='comment-img'></div>
+                    </div>
                 </div>
             </div>
+        )
+    }
+
+    const popWhenNoToken = () => {
+        return (
+            <div className='nav-main'>
+                <div className='nav nav-left'>
+                    <div>
+                        <img className='small-img' src={hamburger} alt=''/>
+                    </div>
+                </div>
+                <div className='nav nav-mid'>
+                    <Link to={'/'}>
+                        <div className='mid-block'>
+                            <p>Blog</p>
+                        </div>
+                    </Link>
+                </div>
+                <div className='nav nav-right'>
+                    <div className='nav-login-div'>
+                        <Link to={'/login'}>
+                            <button className='nav-login-btn'>Login</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className='nav-empty'>
+            {props.hasToken == true ? popWhenToken() : popWhenNoToken()}
         </div>
     )
 }
