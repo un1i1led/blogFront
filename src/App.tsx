7170d34d3475ab196ec920cc323565335a12de5d
gil <gsqwow@gmail.com>
2023-10-08 22:57:12 -0700

    Add hasToken func and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css'
 import Home from './components/Home';
 import Nav from './components/Nav';
@@ -7,12 +8,18 @@ import Login from './components/Login';
 import SignUp from './components/SignUp';
 
 function App() {
+  const [hasToken, setHasToken] = useState(false);
+
+  const changeToken = (value: boolean) => {
+    setHasToken(value);
+  }
+
   return (
     <Router>
       <div className='App'>
-        <Nav/>
+        <Nav hasToken={hasToken}/>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='/' element={<Home changeToken={changeToken}/>}/>
           <Route path='/posts/:postid' element={<ArticleDetail/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/signup' element={<SignUp/>}/>
