be9f6c77aa984fa82b51cc8ddc8ec1251827916d
gil <gsqwow@gmail.com>
2023-10-17 23:24:57 -0700

    Add func to Nav to check user token

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,43 @@
 import notifImg from '../assets/notification-14-svgrepo-com.svg';
 import hamburger from '../assets/hamburger-svgrepo-com.svg';
 import { Link, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 interface NavDetails {
     hasToken: boolean;
     username: string;
     controlSidebar: () => void;
+    changeToken: (value: boolean) => void;
+    changeUsername: (usrname: string) => void;
 }
 
 const Nav = (props: NavDetails) => {
     const location = useLocation();
+
+    useEffect(() => {
+        const checkToken = async () => {
+            await fetch('http://localhost:3000/token', {
+              method: 'GET',
+              headers: {
+                'Authorization': `Bearer ${localStorage.getItem('userToken')}`,
+                'Content-Type': 'application/json'
+              }
+            })
+              .then((res) => res.json())
+              .then((res) => {
+                if (res.msg == 'authorized') {
+                  props.changeToken(true);
+                  props.changeUsername(res.username);
+                } else {
+                  localStorage.removeItem('userToken');
+                  props.changeToken(false);
+                  props.changeUsername('');
+                }
+              });
+        }
+      
+        checkToken();
+    }, [])
 
     const popWhenToken = () => {
         return (
